58b33d4cee5d30cca73a58db88309a6682e11138
zero-rei <r.sasaki.sys19@morijyobi.ac.jp>
2020-06-23 10:58:47 +0900

    moru

--- insert.php
+++ insert.php
@@ -5,22 +5,17 @@ $password = 'Gamezero0';
 try {
     $dbh = new PDO($dsn, $user, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $id= $_POST['id'];
-    $name= $_POST['name'];
-    $age= $_POST['age'];
-
-    $sql = "insert into user values (:id, :name, :age)";
-    $stmt=$dbh->prepare($sql);
-    $params=array(':id' =>$id, ':name' =>$name, ':age' =>$age)
-     
-    $stmt->execute($params);
-
-    header('Location: index.php?fg=1');
-
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $sql = "insert into user values(:id, :name, :age)";
+    $stmt = $dbh->prepare($sql);
+    $params = array(':id' => $id, ':name' => $name, ':age' => $age,);
+    $result = $dbh -> query($sql);
+    header('Location: index.php?fg = 1');
 } catch (PDOException $e) {
-    echo "接続失敗: " . $e->getMessage() . "\n";
-    header('Location: index.php?fg=1?err=$e->getMessage()');
+    //echo "接続失敗: " . $e->getMessage() . "\n";
+    header('Location: index.php?fg = 1? err=$e -> getMessage()');
     exit();
 }
 ?>
